9a5fc59d4b83e188b117e4f844003918704c5980
0SpaceTraveler0 <t34ilya2014@mail.ru>
2022-05-23 12:04:46 +0500

    test

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class=" app">   
+        <post-form @create = "createPost" />
+        <post-list :posts="posts" @delete = "deletePost" /> 
+    </div>
+</template> 
+<script>
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+export default{
+    components:{
+    PostList,
+    PostForm
+}, 
+    data() {
+        return{
+            posts:[
+               {id:1, name: 'Тест Тостович', phone: '8 (800) 555 35-35', body: ' Банк'},
+               {id:2, name: 'Илья Палыч', phone: '8 (800) 555 36-36', body: ' Щегол'},
+               {id:3, name: 'Илья Палыч', phone: '8 (800) 555 36-36', body: ' Джун'}
+            ],
+        }
+    }, 
+    methods:{
+        createPost(post){
+            this.posts.push(post);
+        },
+        deletePost(post){
+            /*this.posts.splice(post);*/
+            this.posts.splice(this.posts.indexOf(post), 1);
+        }
+    }
+}
+</script>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app{
+    padding: 20px;
+}
+
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class = 'post' v-for="post in posts" :key="post">
+        <div><strong>Имя:</strong> {{post.name}}</div>
+        <div><strong>Телефон:</strong>  {{post.phone}}</div>
+        <div><strong>Кем приходиться:</strong>  {{post.body}}</div>
+        <button class="btn" @click="deletePost(post)">Удалить</button>
+        <button class="btn">Редактировать</button>
+    </div>
+</template>
+<script>  
+export default{
+    props: {
+        posts:{
+            type: Array,
+            required: true
+        }
+    },
+    methods:{
+        deletePost(post){
+            this.$emit('delete', post) 
+        }
+    }
+}
+</script>
+<style scoped>
+.post{
+    margin: 5px;
+    padding: 15px;
+    border: 2px solid teal;    
+}
+.btn{
+    margin: 15px 2px;
+    align-self:  flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
+.btn:hover{
+    background: teal;
+    color: white;
+}
+</style>
+ 
